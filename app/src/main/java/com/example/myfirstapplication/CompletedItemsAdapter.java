d38a03a12f3e09d69010824eb282e61ef987dd4e
Clark Chan <chan239@purdue.edu>
2019-06-23 23:37:41 -0700

    Added popups for the completed items as well.

--- app/src/main/java/com/example/myfirstapplication/CompletedItemsAdapter.java
+++ app/src/main/java/com/example/myfirstapplication/CompletedItemsAdapter.java
@@ -1,23 +1,28 @@
 package com.example.myfirstapplication;
 
+import android.app.Dialog;
 import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class CompletedItemsAdapter extends ArrayAdapter<Item> {
     private MainActivity context;
     private int template_resource;
+    private ArrayList<Item> completedItems;
 
-    public CompletedItemsAdapter(MainActivity context, int resource, ArrayList<Item> items) {
-        super(context, resource, items);
+    public CompletedItemsAdapter(MainActivity context, int resource, ArrayList<Item> completedItems) {
+        super(context, resource, completedItems);
         this.context = context;
+        this.completedItems = completedItems;
         template_resource = resource;
     }
 
@@ -35,17 +40,15 @@ public class CompletedItemsAdapter extends ArrayAdapter<Item> {
 
         //set name and the rolling text
         name.setText(currentItem.getName());
-        //name.setTypeface(context.professionalFont);
-        name.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        name.setEllipsize(TextUtils.TruncateAt.END);
         name.setSingleLine(true);
-        name.setMarqueeRepeatLimit(-1);
-        name.setFocusableInTouchMode(true);
-        name.setFocusable(true);
         name.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView itemName = (TextView) v;
-                itemName.setSelected(true);
+                showPopup(position);
+
+//                TextView itemName = (TextView) v;
+//                itemName.setSelected(true);
             }
         });
 
@@ -81,4 +84,54 @@ public class CompletedItemsAdapter extends ArrayAdapter<Item> {
         }
         return convertView;
     }
+
+    public String getTimeString(Item currentItem){
+        Calendar currentItemTime = Calendar.getInstance();
+        currentItemTime.setTimeInMillis(currentItem.getTimeStamp());
+        int dueHour = currentItemTime.get(Calendar.HOUR_OF_DAY);
+        int dueMinute = currentItemTime.get(Calendar.MINUTE);
+        String itemTime = "";
+        if (dueHour % 12 == 0) {
+            itemTime = 12 + ":";
+        } else {
+            itemTime = (dueHour % 12) + ":";
+        }
+        if (dueMinute < 10) {
+            itemTime += "0";
+        }
+        itemTime += dueMinute;
+        if (dueHour >= 12) {
+            itemTime += " PM";
+        } else {
+            itemTime += " AM";
+        }
+        return itemTime;
+    }
+
+    public void showPopup(int position){
+        final Dialog itemPopup = new Dialog(context);
+        itemPopup.setContentView(R.layout.item_popup);
+        ImageView close = (ImageView) itemPopup.findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemPopup.dismiss();
+            }
+        });
+        TextView name = (TextView)itemPopup.findViewById(R.id.name);
+        TextView category= (TextView)itemPopup.findViewById(R.id.category);
+        TextView dueDate= (TextView)itemPopup.findViewById(R.id.dueDate);
+        TextView dueTime= (TextView)itemPopup.findViewById(R.id.dueTime);
+        name.setText(completedItems.get(position).getName());
+        category.setText(completedItems.get(position).getCategory());
+
+        Item currentItem = completedItems.get(position);
+        Calendar itemdueDate = Calendar.getInstance();
+        itemdueDate.setTimeInMillis(currentItem.getTimeStamp());
+        String dueDateText = DateFormat.getDateInstance().format(itemdueDate.getTime());
+        dueDate.setText(dueDateText);
+        dueTime.setText(getTimeString(currentItem));
+        itemPopup.show();
+    }
+
 }
